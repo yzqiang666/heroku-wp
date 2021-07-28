b9dd66d21cb76e20369606a2f25c677a37bba03f
yzqiang666 <yzqiang6@gmail.com>
2021-07-28 18:39:06 +0800

    Update file.php

--- public/file.php
+++ public/file.php
@@ -38,24 +38,24 @@ function read_all ($dir){
 			$temp = $dir.DIRECTORY_SEPARATOR.$fl;
 			$ext=strtolower(substr(strrchr($fl, '.'), 1));
 			$files[$cur]='';
-			if(is_dir($temp))    if (substr($fl,0,1)!='.') { $files[$cur] = '0'.$fl;   $fs_count[0]++; }
-			if ($ext=='mp4' || $ext=='mkv' || $ext=='avi' || $ext=='wmv' || $ext=='mov' || $ext=='ts')  { $files[$cur] = '1'.$fl; $fs_count[1]++; }
-			if ($ext=='wav' || $ext=='mp3' || $ext=='flc' || $ext=='ape')   { $files[$cur] = '2'.$fl; $fs_count[2]++; }
-			if ($ext=='jpg' || $ext=='bmp' || $ext=='png' || $ext=='pcx')  { $files[$cur] = '3'.$fl;  $fs_count[3]++; }
-			if ($ext=='apk' || $ext=='exe' || $ext=='com' || $ext=='ipk')  { $files[$cur] = '4'.$fl;  $fs_count[4]++; }
-			if ($ext=='zip' || $ext=='rar' || $ext=='tar' || $ext=='gz')  { $files[$cur] = '5'.$fl;  $fs_count[5]++; }			
-			if ($ext=='txt' || $ext=='ini' || $ext=='inf' || $ext=='doc' || $ext=='docx' || $ext=='xls' || $ext=='xlsx')  { $files[$cur] = '6'.$fl; $fs_count[6]++;  }					
-//			if ($ext=='html' || $ext=='php' || $ext=='asp' )  { $files[$cur] = '7'.$fl;  $fs_count[7]++; }		
+			if(is_dir($temp))    if (substr($fl,0,1)!='.') { @$files[$cur] = '0'.$fl;   @$fs_count[0]++; }
+			if ($ext=='mp4' || $ext=='mkv' || $ext=='avi' || $ext=='wmv' || $ext=='mov' || $ext=='ts')  { @$files[$cur] = '1'.$fl; @$fs_count[1]++; }
+			if ($ext=='wav' || $ext=='mp3' || $ext=='flc' || $ext=='ape')   { @$files[$cur] = '2'.$fl; @$fs_count[2]++; }
+			if ($ext=='jpg' || $ext=='bmp' || $ext=='png' || $ext=='pcx')  { @$files[$cur] = '3'.$fl;  @$fs_count[3]++; }
+			if ($ext=='apk' || $ext=='exe' || $ext=='com' || $ext=='ipk')  { @$files[$cur] = '4'.$fl;  @$fs_count[4]++; }
+			if ($ext=='zip' || $ext=='rar' || $ext=='tar' || $ext=='gz')  { @$files[$cur] = '5'.$fl;  @$fs_count[5]++; }			
+			if ($ext=='txt' || $ext=='ini' || $ext=='inf' || $ext=='doc' || $ext=='docx' || $ext=='xls' || $ext=='xlsx')  { @@$files[$cur] = '6'.$fl; @$fs_count[6]++;  }					
+			if ($ext=='html' || $ext=='php' || $ext=='asp' )  { @$files[$cur] = '7'.$fl;  @$fs_count[7]++; }		
 
 			
-//			if ($files[$cur]=='') {$files[$cur] = 'z'.$fl; $fs_count[29]++; }
+			if ($files[$cur]=='') {@$files[$cur] = 'z'.$fl; @$fs_count[29]++; }
 			$cur = $cur+1;
         }
     
 		sort($files,0);
 		echo '<tr><td>0</td><td>种类</td><td>';
-//		for ($x=0;$x<29;$x++) if ($fs_count[$x]>0){ echo $x.':'.$fs_count[$x].' '; }
-//		echo '</td></tr>';
+		for ($x=0;$x<29;$x++) if ($fs_count[$x]>0){ echo $x.':'.@$fs_count[$x].' '; }
+		echo '</td></tr>';
 		for($x=0;$x<$cur;$x++) {
 			$fl = substr($files[$x],1);
 			$temp = $dir.DIRECTORY_SEPARATOR.$fl;
