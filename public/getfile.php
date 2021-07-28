269fbdc883cf434caf09defafe9e04623dbc1a53
yzqiang666 <yzqiang6@gmail.com>
2021-07-28 18:19:43 +0800

    Update getfile.php

--- public/getfile.php
+++ public/getfile.php
@@ -1,15 +1,20 @@
 <?php
-$fn=$_GET["fn"];
-
+@$fn=$_GET["fn"];
+if ($fn=="") 
+{
+  echo "Usage: getfile.php?fn=xxx.txt -F file=@xxx.txt \n";
+}
+else
+{
 if ($_FILES["file"]["error"] > 0)
   {
-  echo "Error: " . $_FILES["file"]["error"."\n"];
+  echo "Error: " . $_FILES["file"]["error"]."\n";
   }
 else
   {
   if ($fn=="") $fn=$_FILES["file"]["name"];
-  echo "upload file [" . $fn . "] " . ($_FILES["file"]["size"]) . " bytes.\n";
+  echo "upload file [" . $fn . "] " . ($_FILES["file"]["size"]) . " bytes\n";
   move_uploaded_file($_FILES["file"]["tmp_name"],"$fn");
   }
+}
 ?>
-
